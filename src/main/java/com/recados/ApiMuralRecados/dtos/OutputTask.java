1cbf4fdf8b2525129d87bf0c59d85d8358e479fb
Carla Hentschel <carlarhentschel@gmail.com>
2023-06-21 12:09:43 -0300

    adicionado id no OutputTask

--- src/main/java/com/recados/ApiMuralRecados/dtos/OutputTask.java
+++ src/main/java/com/recados/ApiMuralRecados/dtos/OutputTask.java
@@ -3,16 +3,18 @@ package com.recados.ApiMuralRecados.dtos;
 import com.recados.ApiMuralRecados.models.UserTask;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record OutputTask(
         String title,
         String description,
         LocalDate date,
         Boolean favorite,
-        Boolean finished
+        Boolean finished,
+        UUID idTask
 ) {
     public OutputTask(UserTask task) {
-        this(task.getTitle(), task.getDescription(), task.getDate(), task.isFavorite(), task.isFinished());
+        this(task.getTitle(), task.getDescription(), task.getDate(), task.isFavorite(), task.isFinished(), task.getId());
     }
 
 }
