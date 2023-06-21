ad1407ef249a145b8438d82546bb944a8ee90d8b
Carla Hentschel <carlarhentschel@gmail.com>
2023-06-21 14:25:58 -0300

    adicionado id no UpdateTask

--- src/main/java/com/recados/ApiMuralRecados/dtos/UpdateTask.java
+++ src/main/java/com/recados/ApiMuralRecados/dtos/UpdateTask.java
@@ -1,12 +1,14 @@
 package com.recados.ApiMuralRecados.dtos;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record UpdateTask(
         String title,
         String description,
         LocalDate date,
         Boolean favorite,
-        Boolean finished
+        Boolean finished,
+        UUID idTask
 ) {
 }
