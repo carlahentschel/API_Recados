f640425628bf40819ccccefb811c319a2e5b7b01
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-02 17:49:29 -0300

    remove querys

--- src/main/java/com/recados/ApiMuralRecados/repositories/UserTaskRepository.java
+++ src/main/java/com/recados/ApiMuralRecados/repositories/UserTaskRepository.java
@@ -1,16 +1,10 @@
 package com.recados.ApiMuralRecados.repositories;
 
-import com.recados.ApiMuralRecados.models.User;
 import com.recados.ApiMuralRecados.models.UserTask;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 import java.util.UUID;
 
 public interface UserTaskRepository extends JpaRepository<UserTask, UUID>, JpaSpecificationExecutor<UserTask> {
-    //List<UserTask> findAllByUserId(UUID idUser);
-    //List<UserTask> findAllByUserId(UUID idUser, Specification<UserTask> specification );
 
 }
